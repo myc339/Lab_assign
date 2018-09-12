e8845a63901f4a76f18dfe9babf84e55b805f6da
YumengChen <chen.yum@husky.neu.edu>
2018-09-12 10:08:22 +0800

    second

---
+++ test2.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var data = {name:'stevin',location:'Beijing'};
+var srv = http.createServer(function(req,res) {
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.end(JSON.stringify(data));
+});
+
+srv.listen(8080,function(){
+    console.log('listening on localhost:8080');
+});
